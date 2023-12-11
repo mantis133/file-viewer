9e24e1b6b66d596ee9e444dfe75977f91a35353a
mantis133 <mantisshrimp40@gmail.com>
2023-12-11 21:13:13 +1300

    test

--- src/utils/file_type.rs
+++ src/utils/file_type.rs
@@ -29,7 +29,7 @@ pub enum FileType{
 // all encompassing type
     UnDefinded
 }
-
+ 
 
 impl std::fmt::Display for FileType{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
